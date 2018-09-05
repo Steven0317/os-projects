180548d69929d5299e991af3837d65b8b36ef879
steven <sfaulkner1@gmail.com>
2018-09-05 16:16:35 -0400

    bug fixes

--- project1.c
+++ project1.c
@@ -13,26 +13,110 @@
 #include <unistd.h>
 #include <errno.h>
 
-void ChildProcess1(int *a);
-void ChildProcess2(int *a);
-void ChildProcess3(int *a);
-void ChildProcess4(int *a);
-
 typedef struct
 {
 	int value;
-} shared_memory;
+}shared_memory;
 
 shared_memory *total;
 
+
+void ChildProcess1(shared_memory *a)
+{
+	a->value += 1000000;
+	printf("From Process 1: counter = %d\n", a->value);
+}
+void ChildProcess2(shared_memory *a)
+{
+	a->value += 1000000;
+	printf("From Process 2: counter = %d\n", a->value);
+}
+void ChildProcess3(shared_memory * a)
+{
+	a->value += 1000000;
+	printf("From Process 3: counter = %d\n", a->value);
+}
+void ChildProcess4(shared_memory *a)
+{
+	a->value += 1000000;
+	printf("From Process 3: counter = %d\n", a->value);
+}
 
 int main(void)
 {
 
 	int shmid;
 	pid_t pid;
+	int status;
+
+	shmid = shmget(IPC_PRIVATE,sizeof(int),IPC_CREAT | 0666);
+	if (shmid < 0 )
+	{
+		printf("shmget error\n");
+		exit(0);
+	}
+
+	total =  shmat(shmid,NULL,0);
+	if((int) total->value == -1)
+	{
+		printf("shmat error\n");
+		exit(1);
+	}
+
+	total->value = 0;
+
+	pid = fork();
+	if(pid < 0)
+	{
+		printf("fork error 1\n");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		ChildProcess1(total);
+		exit(0);
+	}
+
+
+	pid = fork();
+	if(pid < 0)
+	{
+		printf("fork error 2\n");
+		exit(1);
+	}
+	else if(pid == 0)
+	{
+		ChildProcess2(total);
+		exit(0);
+	}
+
+
+	pid = fork();
+	if(pid < 0)
+	{
+		printf("fork error 3\n");
+		exit(1);
+	}
+	else if( pid == 0)
+	{
+		ChildProcess3(total);
+		exit(0);
+	}
+
+
+	pid = fork();
+	if(pid < 0)
+	{
+		printf("fork error 4\n");
+		exit(1);
+	}
+	else if(pid == 0)
+	{
+		ChildProcess4(total);
+		exit(0);
+	}
 
 	
-	return 0;
+	exit(0);
 }
 
