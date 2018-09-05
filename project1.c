e61b681d153e751ed6960ac20cbd720d3b2aee43
Steven <sfaulkner1@gmail.com>
2018-09-05 00:34:08 -0400

    "Added project 1 file"

---
+++ project1.c
@@ -0,0 +1,30 @@
+/*
+ *	Author: Steven Faulkner
+ *	Project 1 for COP4600
+ *	
+ */
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys.ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+// key number
+#define SHMKEY ((key_t) 1497);
+
+typedef struct
+{
+	int value;
+} shared_memory;
+
+shared_memory *total;
+
+int main(void)
+{
+	return 0;
+}
+
