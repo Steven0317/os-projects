4b8d6ccff580945880031e6f27c759e18c77be76
steven <sfaulkner1@gmail.com>
2018-09-08 06:47:38 -0400

    Fixed shared memory release

--- project1.c
+++ project1.c
@@ -7,12 +7,14 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct
 {
@@ -24,30 +26,36 @@ shared_memory *total;
 
 void ChildProcess1(shared_memory *a)
 {
-	a->value += 1000000;
+	a->value += rand() %(10000 + 1 - 0) + 0;
 	printf("From Process 1: counter = %d\n", a->value);
 }
 void ChildProcess2(shared_memory *a)
 {
-	a->value += 1000000;
+	a->value += rand() % (20000 + 1 - 0) + 0;
 	printf("From Process 2: counter = %d\n", a->value);
 }
 void ChildProcess3(shared_memory * a)
 {
-	a->value += 1000000;
+	a->value += (30000 + 1 - 0) + 0;
 	printf("From Process 3: counter = %d\n", a->value);
 }
 void ChildProcess4(shared_memory *a)
 {
-	a->value += 1000000;
-	printf("From Process 3: counter = %d\n", a->value);
+	a->value += (50000 + 1 - 0) + 0;
+	printf("From Process 4: counter = %d\n", a->value);
 }
 
-void main(void)
+int main(void)
 {
 
+
+
 	int shmid;
-	pid_t pid;
+	pid_t pid1, pid2, pid3, pid4;
+	time_t t;
+	
+	
+	srand((unsigned) time(&t));
 
 	shmid = shmget(IPC_PRIVATE,sizeof(int),IPC_CREAT | 0666);
 	if (shmid < 0 )
@@ -56,6 +64,8 @@ void main(void)
 		exit(0);
 	}
 
+
+
 	total =  shmat(shmid,NULL,0);
 	if((int) total->value == -1)
 	{
@@ -63,67 +73,69 @@ void main(void)
 		exit(1);
 	}
 
+	total->value = 0;
 
 
-	pid = fork();
-	if(pid < 0)
+	pid1 = fork();
+	if(pid1 < 0)
 	{
 		printf("fork error 1: %s\n", strerror(errno));
 		exit(1);
 	}
-	else if (pid == 0)
+	else if (pid1 == 0)
 	{
 		ChildProcess1(total);
 		exit(0);
 	}
 
 
-	pid = fork();
-	if(pid < 0)
+	pid2 = fork();
+	if(pid2 < 0)
 	{
 		printf("fork error 2: %s\n", strerror(errno));
 		exit(1);
 	}
-	else if(pid == 0)
+	else if(pid2 == 0)
 	{
 		ChildProcess2(total);
 		exit(0);
 	}
 
 
-	pid = fork();
-	if(pid < 0)
+	pid3 = fork();
+	if(pid3 < 0)
 	{
 		printf("fork error 3: %s\n", strerror(errno));
 		exit(1);
 	}
-	else if( pid == 0)
+	else if(pid3 == 0)
 	{
 		ChildProcess3(total);
 		exit(0);
 	}
 
 
-	pid = fork();
-	if(pid < 0)
+	pid4 = fork();
+	if(pid4 < 0)
 	{
 		printf("fork error 4: %s\n", strerror(errno));
 		exit(1);
 	}
-	else if(pid == 0)
+	else if(pid4 == 0)
 	{
 		ChildProcess4(total);
 		exit(0);
 	}
 
-	printf("Child with ID: %d exited\n", wait(NULL));
-	printf("Child with ID: %d exited\n", wait(NULL));
-	printf("Child with ID: %d exited\n", wait(NULL));
-	printf("Child with ID: %d exited\n", wait(NULL));
+	printf("Child with ID: %d exited\n", waitpid(pid1,NULL,0));
+	printf("Child with ID: %d exited\n", waitpid(pid2,NULL,0));
+	printf("Child with ID: %d exited\n", waitpid(pid3,NULL,0));
+	printf("Child with ID: %d exited\n", waitpid(pid4,NULL,0));
 
-	shmdt((void *) total->value);
+	shmdt((void *) total);
 	shmctl(shmid,IPC_RMID,NULL);
 
+	printf("End of Program\n");	
 
 	exit(0);
 }
